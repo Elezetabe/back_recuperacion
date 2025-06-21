6b1066ffdd2c6e71d2a0c02d6fef777eb67fb20f
Elezetabe <laurita_9505@hotmail.com>
2025-06-21 11:24:18 -0500

    Agregando más seeder

--- database/seeders/libro_seeder.ts
+++ database/seeders/libro_seeder.ts
@@ -34,6 +34,54 @@ export default class extends BaseSeeder {
         editorial: 'Ballantine Books',
         fecha_publicacion: 1953,
       },
+      {
+        titulo: 'El túnel',
+        autor: 'Ernesto Sabato',
+        editorial: 'Editorial Sur',
+        fecha_publicacion: 1948,
+      },
+      {
+        titulo: 'Rayuela',
+        autor: 'Julio Cortázar',
+        editorial: 'Editorial Sudamericana',
+        fecha_publicacion: 1963,
+      },
+      {
+        titulo: 'La sombra del viento',
+        autor: 'Carlos Ruiz Zafón',
+        editorial: 'Planeta',
+        fecha_publicacion: 2001,
+      },
+      {
+        titulo: 'Crónica de una muerte anunciada',
+        autor: 'Gabriel García Márquez',
+        editorial: 'Editorial Oveja Negra',
+        fecha_publicacion: 1981,
+      },
+      {
+        titulo: 'El Aleph',
+        autor: 'Jorge Luis Borges',
+        editorial: 'Emecé Editores',
+        fecha_publicacion: 1949,
+      },
+      {
+        titulo: 'El principito',
+        autor: 'Antoine de Saint-Exupéry',
+        editorial: 'Reynal & Hitchcock',
+        fecha_publicacion: 1943,
+      },
+      {
+        titulo: 'El amor en los tiempos del cólera',
+        autor: 'Gabriel García Márquez',
+        editorial: 'Editorial Oveja Negra',
+        fecha_publicacion: 1985,
+      },
+      {
+        titulo: 'Los detectives salvajes',
+        autor: 'Roberto Bolaño',
+        editorial: 'Anagrama',
+        fecha_publicacion: 1998,
+      },
     ])
   }
 }
